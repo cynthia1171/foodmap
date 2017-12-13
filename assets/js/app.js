934d523d42b2e48937179c85d9eef51cfa7bc0ff
cynthia1171 <c.isla1171@gmail.com>
2017-12-12 21:45:06 -0300

    subiendo html, css y algunas funciones

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+  /*splash*/
+  setTimeout(function () {
+    $(".splash").fadeOut(500);
+  }, 4000);
+  setTimeout(function () {
+    $(".principal").fadeIn(500);
+  });
+
+});
+/*
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 16
+  });
+  var infoWindow = new google.maps.InfoWindow({ map: map });
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Aquí estoy.');
+      map.setCenter(pos);
+    }, function () {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+}*/
---
+++ assets/js/data.js
@@ -0,0 +1,29 @@
+var comidas = {
+  'chatarra':[
+    {
+      nombre: 'McDonalds',
+      direccion: 'Av. José Pedro Alessandri 1165, Ñuñoa',
+      imagen: 'assets/img/chatarra/mcdonalds.jpg'
+    },{
+      nombre: 'KFC',
+      direccion: 'Estado 129, Santiago',
+      imagen: 'assets/img/chatarra/kfc.jpg'
+    },{
+      nombre: 'Pedro, Juan y Diego',
+      direccion: 'Mall Paseo San Bernardo',
+      imagen: 'assets/img/chatarra/pjd.jpg'
+    },{
+      nombre: 'Doggis',
+      direccion: 'Av. Vicuña Mackenna Ote. 7110, La Florida',
+      imagen: 'assets/img/chatarra/doggis.jpg'
+    },{
+      nombre: 'Juan Maestro',
+      direccion: '5to piso, Av. José Pedro Alessandri 1166, Ñuñoa',
+      imagen: 'assets/img/chatarra/juanMaestro.jpg'
+    },{
+      nombre: 'Los Pincheira',
+      direccion: 'Alcalde Jorge Monckeberg 756, Ñuñoa',
+      imagen: 'assets/img/chatarra/pincheira.jpg'
+    }
+  ]
+}
